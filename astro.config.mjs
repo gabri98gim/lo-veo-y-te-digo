c8ef57f6ad7a14fb3829be6e87fc6ac362e9e800
gabri98gim <gandreu8@gmail.com>
2026-01-29 20:55:20 +0100

    Activado SSR con Vercel para fechas dinámicas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/server';
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [
     react(),
     tailwind()
